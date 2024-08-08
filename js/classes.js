575d60d28ddcefe91f991debfb8982030f84ea88
NicollasLems <nicollas.matos@etec.sp.gov.br>
2024-08-08 12:19:57 -0300

    atualização

--- js/classes.js
+++ js/classes.js
@@ -1,3 +1,23 @@
-class obj{
-    
+class obj{ //constroi o obj
+    constructor(px, py, lar, alt, imagem){
+        this.px = px
+        this.py = py
+        this.lar = lar
+        this.alt = alt
+        this.imagem = imagem
+    }
+    draw(){
+        let img = new Image()
+        img.src = this.imagem
+        canvas.drawImage(img, this.px, this.py, this.lar, this.alt)
+    }
+}
+
+class BG extends obj{
+    move(velocidade, limite, pos){
+        this.px -= velocidade
+        if(this.px < -limite){
+            this.px = pos
+        }
+    }
 }
